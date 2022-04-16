f18efaed2e0f3b1f566d5b741a8e90cd631e0aad
zwtop <wang.zhan@smartx.com>
2022-04-16 21:32:52 +0800

    feat: ch02
    Signed-off-by: zwtop <wang.zhan@smartx.com>

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,33 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("Guess the number!");
+
+    loop {
+        let mut guess = String::new();
+        println!("Please input your guess.");
+
+        // Read the guess from the user
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+            Ordering::Greater => println!("Too big!"),
+        }
+    }
+}
